17dcd80972581ecbe2a87eac43274958f6afb799
Avokazzar <65611836+Avokazzar@users.noreply.github.com>
2020-11-18 22:09:16 +0300

    Update Ex_3.cpp

--- Ex_3.cpp
+++ Ex_3.cpp
@@ -1,14 +1,17 @@
 #include <iostream>
 
+using std::cout;
+using std::cin;
+
 int main()
 {
     int a, b;
-    std::cout << "Input:\n";
-    std::cin >> a >> b;
-    std::cout << "Output:\n";
+    cout << "Input:\n";
+    cin >> a >> b;
+    cout << "Output:\n";
     for (int i = a; i > 0; i--) {
         if (a % i == 0 && b % i == 0) {
-            std::cout << i;
+            cout << i;
             break;
         }
     }
