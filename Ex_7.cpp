0cde10732f5c7dd81229b3f47d174a865e2b2693
Avokazzar <65611836+Avokazzar@users.noreply.github.com>
2020-11-18 21:57:13 +0300

    Create Ex_7.cpp

---
+++ Ex_7.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+
+int main()
+{
+    long int f1 = 1, f2 = 2, f = 0;
+    long long sum = 2;
+    do {
+        f = f1 + f2;
+        if (f % 2 == 0) {
+            sum += f;
+        }
+        f1 = f2;
+        f2 = f;
+    } while (f <= 4000000);
+
+    std::cout << "Output:\n" << sum;
+}
