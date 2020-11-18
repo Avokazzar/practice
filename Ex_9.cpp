0393c79fa778dde5a0b4d4c2cac44399d0c52d17
Avokazzar <65611836+Avokazzar@users.noreply.github.com>
2020-11-18 21:58:29 +0300

    Create Ex_9.cpp

---
+++ Ex_9.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+#include <math.h>
+
+int main()
+{
+    int sum_sq = 0, sq_sum = 0, dif;
+    for (int i = 1; i <= 100; i++) {
+        sum_sq += pow(i, 2);
+        sq_sum += i;
+    }
+    dif = pow(sq_sum, 2) - sum_sq;
+    std::cout << "Output:\n" << dif;
+}
